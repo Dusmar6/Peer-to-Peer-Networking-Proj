350eda9c26b826392f36ffce7fd96bdb121fea9d
Dustin Martin <36971079+Dusmar6@users.noreply.github.com>
2020-08-09 15:36:05 -0700

    Fixed code, should be final

--- file.py
+++ file.py
@@ -3,30 +3,9 @@ class File:
         self.name = name
         self.path = path
         self.mod = mod
-        
-        
+
     def __str__(self):
         ret = ''
-        ret = ret+ "Name: "+ str(self.name) + "\nPath: "+ str(self.path) + "\nMod: "+ str(self.mod)
+        ret = ret + "Name: " + str(self.name) + "\nPath: " + str(self.path) + "\nMod: " + str(self.mod)
         print(ret)
         return ret
-        
-
-
-#
-# list = []
-#
-# list.append(File("test", 543543.234534, "testpath"))
-#
-# list.append(File("test", 543543.234534, "testpath"))
-#
-# list.append(File("test", 543543.234534, "testpath"))
-#
-#
-# j = {"masterlist":[]}
-# for file in list:
-#     j["masterlist"].append({"name": file.name, "mod": file.mod,  "path": file.path})
-#
-#
-# print(j)
-    
--- files.py
+++ files.py
@@ -5,6 +5,7 @@ import platform
 import time
 import file
 
+
 def get_config():
     config = configparser.ConfigParser()
     try:
@@ -15,48 +16,56 @@ def get_config():
         config.read('config.ini')
     return config
 
+
 def write_default_config():
     config = configparser.ConfigParser()
-    config['config']= {}
+    config['config'] = {}
     config['config']['working directory'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share')
 
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
-        
+
+
 def get_working_directory():
     config = get_config()
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'share')
 
+
 def set_working_directory(path):
     config = get_config()
-    config['config']['working directory']= path
-            
+    config['config']['working directory'] = path
+
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
-        
+
+
 def get_file_names():
     files = os.listdir(get_working_directory())
-    
+
     if len(files) == 0:
         files.append(' ')
 
     return files
 
+
 def get_filepath(name):
     return os.path.join(get_working_directory(), name)
 
+
 def get_mod_time(filepath):
-    fileStatsObj = os.stat ( filepath )
+    fileStatsObj = os.stat(filepath)
     return fileStatsObj.st_mtime
-    
+
+
 def newest_file(filepath1, filepath2):
     time1 = get_mod_time(filepath1)
     time2 = get_mod_time(filepath2)
-    if time1>=time2:
+    if time1 >= time2:
         return filepath1
     else:
         return filepath2
-    
+
+
 def scan():
     list = []
     names = get_file_names()
@@ -65,4 +74,3 @@ def scan():
         mod = get_mod_time(path)
         list.append(file.File(name, mod, path))
     return list
-        
